b2b7979742c9edec119ad950cd4d633e7ab5d3d3
Brian Wong <brianw7218@gmail.com>
2024-10-09 12:16:23 -0700

    Add loading indicator while waiting for backend call

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,16 @@
 import { useQuery } from "@apollo/client";
 import { GetAllPokemonStockPairsDocument } from "../__generated__/graphql";
 import { PSPPreviewCard } from "../components/PSPPreviewCard";
-import { Grid2 as Grid, Stack, Typography } from "@mui/material";
+import {
+  CircularProgress,
+  Container,
+  Grid2 as Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 
 export const MainPage = () => {
-  const { data } = useQuery(GetAllPokemonStockPairsDocument);
+  const { data, loading } = useQuery(GetAllPokemonStockPairsDocument);
 
   return (
     <Stack
@@ -14,7 +20,12 @@ export const MainPage = () => {
       paddingTop={4}
       paddingBottom={6}
     >
-      <Typography variant="h1" textAlign="center" fontWeight={400}>
+      <Typography
+        variant="h1"
+        textAlign="center"
+        fontWeight={400}
+        sx={{ wordWrap: "break-word" }}
+      >
         Pokestocks
       </Typography>
       <Grid
@@ -24,7 +35,7 @@ export const MainPage = () => {
         paddingX={12}
         justifyContent="center"
       >
-        {data &&
+        {!loading && data ? (
           data.pokemonStockPair.map((psp) => {
             return (
               <Grid key={psp.id}>
@@ -35,7 +46,22 @@ export const MainPage = () => {
                 />
               </Grid>
             );
-          })}
+          })
+        ) : (
+          <Container
+            sx={{
+              height: "100vh",
+              display: "flex",
+              justifyContent: "center",
+              width: "200px",
+            }}
+            disableGutters
+          >
+            <Grid display="flex" justifyContent="center">
+              <CircularProgress size={200} />
+            </Grid>
+          </Container>
+        )}
       </Grid>
     </Stack>
   );
