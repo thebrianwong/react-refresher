d3b551f1617658d6bbd99277fc3d4f7a50429183
Brian Wong <brianw7218@gmail.com>
2024-10-10 10:19:01 -0700

    Make the preview card clickable

--- src/components/PSPPreviewCard.tsx
+++ src/components/PSPPreviewCard.tsx
@@ -1,5 +1,6 @@
 import {
   Card,
+  CardActionArea,
   CardContent,
   Grid2 as Grid,
   SxProps,
@@ -26,42 +27,44 @@ export const PSPPreviewCard = ({
   return (
     <>
       <Card>
-        <Grid container direction="row" justifyContent="space-around">
-          <CardContent sx={contentStyling}>
-            <Image
-              height={125}
-              width={125}
-              src={pokemonSpriteUrl}
-              alt={`Official artwork of ${pokemonName}`}
-            />
-            <Typography
-              variant="h6"
-              component="span"
-              fontWeight={600}
-              textAlign="center"
-              marginTop={2}
-            >
-              {pokemonName}
-            </Typography>
-          </CardContent>
-          <CardContent sx={contentStyling}>
-            <Image
-              height={125}
-              width={125}
-              src={`https://logo.synthfinance.com/ticker/${stockSymbol}`}
-              alt={`${stockSymbol} company logo`}
-            />
-            <Typography
-              variant="h6"
-              component="span"
-              fontWeight={600}
-              textAlign="center"
-              marginTop={2}
-            >
-              {stockSymbol}
-            </Typography>
-          </CardContent>
-        </Grid>
+        <CardActionArea>
+          <Grid container direction="row" justifyContent="space-around">
+            <CardContent sx={contentStyling}>
+              <Image
+                height={125}
+                width={125}
+                src={pokemonSpriteUrl}
+                alt={`Official artwork of ${pokemonName}`}
+              />
+              <Typography
+                variant="h6"
+                component="span"
+                fontWeight={600}
+                textAlign="center"
+                marginTop={2}
+              >
+                {pokemonName}
+              </Typography>
+            </CardContent>
+            <CardContent sx={contentStyling}>
+              <Image
+                height={125}
+                width={125}
+                src={`https://logo.synthfinance.com/ticker/${stockSymbol}`}
+                alt={`${stockSymbol} company logo`}
+              />
+              <Typography
+                variant="h6"
+                component="span"
+                fontWeight={600}
+                textAlign="center"
+                marginTop={2}
+              >
+                {stockSymbol}
+              </Typography>
+            </CardContent>
+          </Grid>
+        </CardActionArea>
       </Card>
     </>
   );
